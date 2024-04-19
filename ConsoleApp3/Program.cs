8e6bffed8a6bcedac99893847c95e0bac0a9c0c3
Xxnorsen <153333995+Xxnorsen@users.noreply.github.com>
2024-04-19 14:25:20 +0200

    Add files via upload

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,41 @@
+﻿using ConsoleApp3;
+using System;
+using System.Collections.Generic;
+
+using System.Linq;
+
+using System.Threading.Tasks;
+
+using System.Windows.Forms;
+ 
+namespace ConsoleApp3
+
+{
+
+    static class Program
+
+    {
+
+        /// <summary>
+
+        /// The main entry point for the application.
+
+        /// </summary>
+
+        [STAThread]
+
+        static void Main()
+
+        {
+
+            Application.EnableVisualStyles();
+
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            Application.Run(new Form1());
+
+        }
+
+    }
+
+}
